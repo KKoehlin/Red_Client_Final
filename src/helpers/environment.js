b4b54902b5216f8635614b71a2ff4de8c944f732
Kaylee Koehlinger <kkoehlin@gmail.com>
2021-10-05 11:17:54 -0400

    final push

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:3000';
         break;
         case 'whats-on-tap-app-finalclient.herokuapp.com':
-            APIURL = 'https://whats-on-tap-app-finalserver.herokuapp.com/'
+            APIURL = 'https://whats-on-tap-app-finalserver.herokuapp.com'
 }
 
 export default APIURL; 
